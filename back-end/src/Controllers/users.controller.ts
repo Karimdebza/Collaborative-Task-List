c9f5192ba5ed5238b82055c0b47a67e2fb635b2d
Karim Debza <bwb-k.debza@fondespierre.com>
2024-04-24 16:00:54 +0200

    initial commit

---
+++ back-end/src/Controllers/users.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import pool from "../config/connexion-db";
+import { User } from "../models/users.model";
+
+export async function createUser(req:Request, res:Response): Promise<void> {
+    
+}
+
+export async function getAllUsers(req:Request, res:Response): Promise<void> {
+    
+}
+
+export async function getUserById(req:Request, res:Response): Promise<void> {
+    
+}
+
+export async function updateUser(req:Request, res:Response): Promise<void> {
+    
+}
+
+export async function deleteUser(req:Request, res:Response): Promise<void> {
+    
+}
