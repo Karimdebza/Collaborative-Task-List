c61c6057b3e3cf96d8b45d866f5821316ee43014
Karim Debza <bwb-k.debza@fondespierre.com>
2024-05-01 17:36:25 +0200

    rcreation du formulaire de creation pour les taches

--- front-end/Colaboratif-task/src/app/app.module.ts
+++ front-end/Colaboratif-task/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
-
+import { TaskComponent } from './components/form-task/task.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    TaskListComponent
+    TaskListComponent,
+    TaskComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
     
   ],
   providers: [],
