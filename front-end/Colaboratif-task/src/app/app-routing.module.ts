1310f097c9bcf40aa0b791cf494179b64c670079
Karim Debza <bwb-k.debza@fondespierre.com>
2024-05-03 01:38:31 +0200

    corection

--- front-end/Colaboratif-task/src/app/app-routing.module.ts
+++ front-end/Colaboratif-task/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { TaskListDetailComponent } from './components/task-list/task-list-detail/task-list-detail.component';
+import { TaskComponent } from './components/form-task/task.component';
 const routes: Routes = [
   
   {path:"task-list", component:TaskListComponent},
-  { path: 'task-list/:id', component: TaskListDetailComponent  }
+  { path: 'task-list/:id', component: TaskListDetailComponent  },
+  { path: 'task', component: TaskComponent  }
 ];
 
 @NgModule({
