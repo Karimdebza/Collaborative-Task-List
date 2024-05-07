43f4ecc95bba978fac6fc4d041a2b8c481b67b8b
Karim Debza <bwb-k.debza@fondespierre.com>
2024-05-07 03:10:36 +0200

    utilisation du composant signin tester fonctionnel

--- front-end/Colaboratif-task/src/app/app-routing.module.ts
+++ front-end/Colaboratif-task/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { TaskListComponent } from './components/task-list/task-list.component';
 import { TaskListDetailComponent } from './components/task-list/task-list-detail/task-list-detail.component';
 import { TaskComponent } from './components/form-task/task.component';
 import {ComponentssignupComponent}from './components/signup/componentssignup.component';
+import { SigninComponent } from './components/signin/signin.component';
 const routes: Routes = [
   {path: "", redirectTo:"/signup", pathMatch:"full"},
   {path:"signup", component:ComponentssignupComponent},
+  {path:"signin", component: SigninComponent},
   {path:"task-list", component:TaskListComponent},
   { path: 'task-list/:id', component: TaskListDetailComponent},
   { path: 'task', component: TaskComponent }
---
+++ front-end/Colaboratif-task/src/app/components/signin/signin.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserInterfaceTs } from 'src/app/interface/user.interface.ts';
+import { UserServiceTsService } from 'src/app/services/user.service.ts.service';
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+authForm!:FormGroup;
+
+constructor(private userService:UserServiceTsService, private formBuilder:FormBuilder, private route:Router){}
+
+ngOnInit(): void {
+  this.authForm = this.formBuilder.group({
+    email:['', [Validators.required, Validators.email]],
+    password:['', [Validators.required, Validators.minLength(6)]]
+  });
+  this.authenticateUser();
+}
+
+authenticateUser(): void {
+  if(this.authForm.valid){
+  const userData = this.authForm.value;
+  this.userService.signinUser(userData).subscribe(
+    {next : data => {
+      console.log('authentification reussie :', data);
+      // tomporerment la route task-list quand je cree la page home je fais l'update 
+      this.route.navigate(['/task-list']);
+      
+    },
+    error : error => {
+      console.error("authentification echoué :",error);
+    }}
+  )
+  }
+}
+}
