98a3e0057c80653e6d12cc6727c4898f17d4f236
Karim Debza <bwb-k.debza@fondespierre.com>
2024-05-18 16:39:49 +0200

    Création du formulaire pour les listes des taches les relier a la page task-list

--- front-end/Colaboratif-task/src/app/components/task-list/form-list/form-list.component.ts
+++ front-end/Colaboratif-task/src/app/components/task-list/form-list/form-list.component.ts
@@ -8,24 +8,26 @@ import { TaskListServiceTsService } from 'src/app/services/task-list.service.ts.
 })
 export class FormListComponent {
 listForm!:FormGroup;
+userId:number | null = null;
 
 constructor(private Form:FormBuilder, private ServiceTask:TaskListServiceTsService){}
 
 
 ngOnInit(): void {
+  const userIdString = localStorage.getItem('id_user');
+  console.log('ID de l\'utilisateur :', userIdString); // Vérifier la valeur de l'ID dans la console
+  this.userId = userIdString ? Number(userIdString) : null;
   this.listForm = this.Form.group({
-    name: ['', Validators.required],
-    description: ['', Validators.required],
+    title: ['', Validators.required],
     date_of_create: [new Date(), Validators.required],
-    date_of_expiry: [new Date(), Validators.required],
-    id_task_list: [ , Validators.required]
   })
 }
 
 
-createTask():void {
+createListTask():void {
+  if (typeof this.userId === 'number') {
   if(this.listForm.valid){
-    this.ServiceTask.createTaskList(this.listForm.value,2).subscribe({
+    this.ServiceTask.createTaskList(this.listForm.value,this.userId).subscribe({
       next :data => {
         console.log("create new task");
         
@@ -33,8 +35,12 @@ createTask():void {
       error : error => {
         console.error("error:",error)
       }
+    
     })
   }
+}else{
+  console.error("L'ID de l'utilisateur est null.");
+}
 }
 
 
