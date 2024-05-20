50a15f5da2c98e541feb11bad82d8d61a8de15d6
Karim Debza <bwb-k.debza@fondespierre.com>
2024-05-20 22:09:08 +0200

    ajouter la Méthode de edition  pour les taches

--- front-end/Colaboratif-task/src/app/components/form-task/task.component.ts
+++ front-end/Colaboratif-task/src/app/components/form-task/task.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { TaskServiceTsService } from 'src/app/services/task.service.ts.service';
-import { TaskInterfaceTs } from 'src/app/interface/task.interface.ts';
-import { Route, Router } from '@angular/router';
+// import { TaskInterfaceTs } from 'src/app/interface/task.interface.ts';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
@@ -11,9 +12,13 @@ import { Route, Router } from '@angular/router';
 export class TaskComponent implements OnInit {
 taskForm!:FormGroup;
 taskId: number | null = null;
-constructor(private Form:FormBuilder, private ServiceTask:TaskServiceTsService, private route:Router){}
+userId: number | null = null;
+constructor(private Form:FormBuilder, private ServiceTask:TaskServiceTsService, private router:Router, private route: ActivatedRoute){}
 
 ngOnInit(): void {
+  const userIdString = localStorage.getItem('id_user');
+  console.log('ID de l\'utilisateur :', userIdString); // Vérifier la valeur de l'ID dans la console
+  this.userId = userIdString ? Number(userIdString) : null;
   this.taskForm = this.Form.group({
     name: ['', Validators.required],
     description: ['', Validators.required],
@@ -23,24 +28,36 @@ ngOnInit(): void {
   });
   this.taskId = this.route.snapshot.paramMap.get('id') ? Number(this.route.snapshot.paramMap.get('id')) : null;
   if (this.taskId) {
-    this.taskService.getTask(this.taskId).subscribe(task => {
+    this.ServiceTask.getTaskById(this.taskId).subscribe(task => {
       this.taskForm.patchValue(task);
     });
   }
 }
 
-createTask():void {
+formTask():void {
+  if (typeof this.userId === 'number') {
   if(this.taskForm.valid){
-    this.ServiceTask.createTask(this.taskForm.value,2).subscribe({
+    if(this.userId){
+    this.ServiceTask.updateTask(this.userId,this.taskForm.value).subscribe({
       next :data => {
-        console.log("create new task");
-        
-      },
+        console.log("create new task",data);
+
+        this.router.navigate(['/task-list']);
+    },
       error : error => {
         console.error("error:",error)
       }
-    })
+    });
+  }else{
+    this.ServiceTask.createTask(this.taskForm.value,this.userId).subscribe(() => {
+      console.log('Tâche créée avec succès');
+      this.router.navigate(['/task-list']);
+    });
   }
+  }
+}else{
+  "user id is null"
+}
 }
 
 }
