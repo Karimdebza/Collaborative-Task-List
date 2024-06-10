1b71c79129b23f50220d97f2f25006ac00b66f72
Karim Debza <bwb-k.debza@fondespierre.com>
2024-06-10 18:13:08 +0200

    mise en place du service socket la méthode send notification pour envoyer des notification  la méthode onNotification pour ecouter les evenment de notif et une methode pour fermer la socket

---
+++ front-end/Colaboratif-task/src/app/services/notification.service.ts.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationServiceTsService {
+
+  constructor() { }
+
+  requestPermission(){
+    if ('Notification' in window) {
+    Notification.requestPermission().then((result) => {
+      if( result === 'granted'){
+        console.log("Notification permission garanted");
+        
+      }else{
+        console.log("Notificationpermission denied.");
+        
+      }
+    });
+  }
+  }
+
+  showNotification(title:string, option?:NotificationOptions){
+    if ('Notification' in window) {
+    if(Notification.permission === 'granted'){
+      new Notification(title,option);
+    }
+  }
+}
+}
