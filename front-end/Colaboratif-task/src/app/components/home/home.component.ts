e24fe8bf924018fa679620b6bc86ec9faa5cccaa
Karim Debza <bwb-k.debza@fondespierre.com>
2024-06-15 02:40:09 +0200

    ajouter le caleendrier de googlecalendarPlugin syncornisation avec les taches WIP

--- front-end/Colaboratif-task/src/app/components/home/home.component.ts
+++ front-end/Colaboratif-task/src/app/components/home/home.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import googleCalendarPlugin from '@fullcalendar/google-calendar';
-import { CalendarOptions } from '@fullcalendar/core';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  calendarOptions: CalendarOptions = {
-    plugins: [dayGridPlugin, googleCalendarPlugin],
-    initialView: 'dayGridMonth',
-    googleCalendarApiKey: 'YOUR_GOOGLE_API_KEY',
-    events: {
-      googleCalendarId: 'YOUR_GOOGLE_CALENDAR_ID@group.calendar.google.com'
-    }
-  };
+ 
 }
