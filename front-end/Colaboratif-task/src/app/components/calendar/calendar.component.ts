10f86db2ebee7405e6d12ff54a935a81009d25f2
Karim Debza <bwb-k.debza@fondespierre.com>
2024-06-23 21:25:01 +0200

    modification pour le calandrier

--- front-end/Colaboratif-task/src/app/components/calendar/calendar.component.ts
+++ front-end/Colaboratif-task/src/app/components/calendar/calendar.component.ts
@@ -2,42 +2,49 @@ import { Component, OnInit } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import googleCalendarPlugin from '@fullcalendar/google-calendar';
 import { CalendarOptions } from '@fullcalendar/core';
-import { TaskServiceTsService } from 'src/app/services/task.service.ts.service';
+import { TaskListServiceTsService } from 'src/app/services/task-list.service.ts.service';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-  userId:number | null = null;
+ userId: number | null = null;
   calendarOptions: CalendarOptions = {
     plugins: [dayGridPlugin, googleCalendarPlugin],
     initialView: 'dayGridMonth',
     googleCalendarApiKey: 'karimkey',
-    events: []
+    events: [],
   };
 
-  constructor(private taskService:TaskServiceTsService){}
-
+  constructor(private taskService: TaskListServiceTsService) {}
 
   ngOnInit(): void {
-    const userIdString = localStorage.getItem('id_user');
-  console.log('ID de l\'utilisateur :', userIdString); 
-  this.userId = userIdString ? Number(userIdString) : null;
-  this.loadTasks();
+    this.userId = this.getUserIdFromLocalStorage();
+    this.loadTasks();
   }
 
-  loadTasks(): void{
-    if (typeof this.userId === 'number') {
-    this.taskService.getAllTasks(this.userId).subscribe(tasks => {
-      const events = tasks.map(task => ({
-        title: task.name, 
-        start: task.date_of_create, 
-        end: task.date_of_expiry 
-      }));
-      this.calendarOptions.events = events;
-      
-    })
+  getUserIdFromLocalStorage(): number | null {
+    const userIdString = localStorage.getItem('id_user');
+    return userIdString ? Number(userIdString) : null;
   }
-}
+
+  loadTasks(): void {
+    if (this.userId !== null) {
+      this.taskService.getAllTaskLists(this.userId).subscribe(tasks => {
+        const events = tasks.map(task => ({
+          title: task.title,
+          start: task.date_of_create,
+          
+        }));
+        this.updateCalendarEvents(events);
+      });
+    }
+  }
+
+  updateCalendarEvents(events: any[]): void {
+    this.calendarOptions.events = events;
+  }
 }
